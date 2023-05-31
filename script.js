e7858dd606c2fbc04146489e265cca444245e66f
09step <92272272+09step@users.noreply.github.com>
2023-05-31 18:55:56 +0200

    added second page

--- script.js
+++ script.js
@@ -1,87 +1,101 @@
-//Movement Animation to happen
 const card = document.querySelector(".card");
-const container = document.querySelector(".container");
-
-//Items
-const sneaker = document.querySelector(".main-image");
+const donut = document.querySelector(".main-image");
 const title = document.querySelector(".title");
 const description = document.querySelector(".description");
+const button = document.getElementById('btn-main');
 
 
-//Moving Animation Event
+//Donut dreht sich nach links o. rechts
 card.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-  let yAxis = (window.innerHeight / 2 - e.pageY) * 10;
-  sneaker.style.transition = "1s cubic-bezier(.03,.98,.52,.99)";
-  sneaker.style.transform = `rotateZ(${-xAxis}deg)`;
+  donut.style.transition = "1s cubic-bezier(.03,.98,.52,.99)";
+  donut.style.transform = `rotateZ(${-xAxis}deg)`;
 });
 
 //Animate In
 card.addEventListener("mouseenter", (e) => {
-  card.style.transition = "0.1s ease";
   title.style.transition = "all 0.5s ease";
   title.style.transform = "translateY(7px)";
   description.style.transition = "all 0.5s ease";
   description.style.transform = "translateY(10px)";
 
 });
-//Animate Out
+//Animate Out 
 card.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  donut.style.transform = "translateZ(0px) rotateZ(0deg)";
   title.style.transform = "translateZ(0px)";
   description.style.transform = "translateZ(0px)";
 
 });
 
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
+window.onload = () => {
+  const transition_el = document.querySelector('.transition');
+
+  setTimeout(() => {
+    transition_el.classList.remove('is-active')
+  }, 500)
+}
+
+// Slider
+
+let slider = document.getElementById("rangeSlider");
+let output = document.getElementById("confirmation");
 output.innerHTML = slider.value;
 
 slider.oninput = function () {
   output.innerHTML = this.value;
 }
 
+// Transition
+function zoom() {
+  button.style.transition = '0.5s ease';
+  button.style.transform = 'scale(30)';
+  window.location = './ordered.html'
+  setTimeout(() => {
+    button.style.opacity = 0;
+  }, 500)
+  // setTimeout(() => {
+  //   window.location = './ordered.html'
+  // }, 600)
+  // window.location = './ordered.html'
+}
 
 
-
-
-// const domElements = {
-//   button: false,
-//   icon: false,
-// };
-
-// /*
-//  * sets up necessary dom selections and eventlisteners
-//  */
-// function setup() {
-//   // selecting the button via data attribute
-//   // could be done via selecting an id or a class as well
-//   // but a data attribute is more explicit
-//   domElements.button = document.querySelector("[data-js-button]");
-
-//   domElements.icon = document.querySelector("[data-js-icon]");
-
-//   // just checking if the button is correctly selected
-//   console.log("button: ", domElements.button);
-
-//   // listening to a click event on the button
-//   // executing handleClick() function each time the button is clicked
-//   domElements.button.addEventListener("click", handleClick);
+// const state = {
+//   animations: ['zoom'],
+//   view: 'slide'
 // }
 
-// function handleClick(event) {
-//   console.log("event: ", event);
+// const fade = Vue.component('zoom', {
+//   template: '#page',
+//   methods: {
+//     enter(el, done) {
+//       TweenMax.fromTo(el, 1, {
+//         autoAlpha: 0,
+//         scale: 1.5,
+//       }, {
+//         autoAlpha: 1,
+//         scale: 1,
+//         transformOrigin: '50% 50%',
+//         ease: Power4.easeOut,
+//         onComplete: done
+//       });
+//     },
+//     leave(el, done) {
+//       TweenMax.fromTo(el, 1, {
+//         autoAlpha: 1,
+//         scale: 1,
+//       }, {
+//         autoAlpha: 0,
+//         scale: 0.8,
+//         ease: Power4.easeOut,
+//         onComplete: done
+//       });
+//     }
+//   }
+// })
 
-//   rotate(domElements.icon);
-// }
 
-// function rotate(element) {
-//   // toggles class to element passed via parameter
-//   // class triggers a css animation
-//   element.classList.toggle("is-rotating");
-// }
 
-// // execute setup function
-// setup();
+
+
